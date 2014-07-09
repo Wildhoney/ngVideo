ab9284d54860a9dd1ed4d367ae4ddd2f50a79829
Adam Timberlake <adam.timberlake@gmail.com>
2014-07-09 12:13:10 +0100

    Added node deps

---
+++ Gruntfile.js
@@ -0,0 +1,103 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+
+        /**
+         * @property pkg
+         * @type {Object}
+         */
+        pkg: grunt.file.readJSON('package.json'),
+
+        /**
+         * @property jshint
+         * @type {Object}
+         */
+        jshint: {
+            all: 'components/*.js',
+            options: {
+                jshintrc: '.jshintrc'
+            }
+        },
+
+        /**
+         * @property uglify
+         * @type {Object}
+         */
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> by <%= pkg.author %> created on <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: ['components/*.js'],
+                dest: 'dist/<%= pkg.name %>.min.js'
+            }
+        },
+
+        /**
+         * @property copy
+         * @type {Object}
+         */
+        copy: {
+            main: {
+                files: [
+                    { flatten: true, src: ['components/*.js'], dest: 'dist/ng-video.js' }
+                ]
+            },
+            test: {
+                src: 'components/*.js',
+                dest: 'example/js/vendor/ng-video/ng-video.js'
+            },
+            release: {
+                src: 'releases/<%= pkg.version %>.zip',
+                dest: 'releases/master.zip'
+            }
+
+        },
+
+        /**
+         * @property compress
+         * @type {Object}
+         */
+        compress: {
+            main: {
+                options: {
+                    archive: 'releases/<%= pkg.version %>.zip'
+                },
+                files: [
+                    { flatten: true, src: ['dist/**'], dest: './', filter: 'isFile' }
+                ]
+            }
+        },
+
+        /**
+         * @property jasmine
+         * @type {Object}
+         */
+        jasmine: {
+            pivotal: {
+                src: 'components/*.js',
+                options: {
+                    specs: 'tests/spec.js',
+                    helpers: [
+                        'example/js/vendor/angular/angular.js',
+                        'example/js/vendor/moment/moment.js',
+                        'example/js/vendor/crossfilter/crossfilter.js',
+                        'example/js/vendor/angular-mocks/angular-mocks.js'
+                    ]
+                }
+            }
+        }
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jasmine');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-compress');
+
+    grunt.registerTask('build', ['copy', 'uglify', 'compress']);
+    grunt.registerTask('test', ['jasmine', 'jshint']);
+    grunt.registerTask('default', ['jshint', 'jasmine', 'compress', 'copy', 'uglify']);
+
+};
