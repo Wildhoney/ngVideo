43d8941ef7e47ed5f8873b56be5d1bc668921df8
Adam Timberlake <adam.timberlake@gmail.com>
2014-07-14 17:27:08 +0100

    Removed defer from controllers

--- example/js/controllers/VideoController.js
+++ example/js/controllers/VideoController.js
@@ -43,14 +43,8 @@
         };
 
         // Add some video sources for the player!
-        $timeout(function() {
-            video.addSource('mp4', $scope.videos.first);
-        }, 1000);
-
-        // Add some video sources for the player!
-        $timeout(function() {
-            video.addSource('mp4', $scope.videos.second);
-        }, 2000);
+        video.addSource('mp4', $scope.videos.first);
+        video.addSource('mp4', $scope.videos.second);
 
     });
 
